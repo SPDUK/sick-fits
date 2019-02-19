0213c02238ad783f623d934024677d6536eb1ce4
SPDUK <spdevuk@gmail.com>
2019-02-19 19:40:16 +0000

    set up graphql-yoga server

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,21 @@
-// let's go!
+require('dotenv').config({ path: 'variables.env' });
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+
+// TODO use express middleware to handle cookies (JWT)
+// TODO use express middleware to populate current user
+
+// uses CORS, can only hit endpoint from approved URLs (frontend server)
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL,
+    },
+  },
+  info => {
+    console.log(`Server is now running on port http://localhost:${info.port}`);
+  }
+);
