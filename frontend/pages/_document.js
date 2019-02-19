57e826622c283457db6854cd49d1efe0e436ea76
SPDUK <spdevuk@gmail.com>
2019-02-19 16:57:23 +0000

    fixing flash of unstyled text with custom _document.js

---
+++ frontend/pages/_document.js
@@ -0,0 +1,28 @@
+// lets us avoid the flash of unstyled content by grabbing the styles before
+//  rendering everything else, because we use styled-components this is needed
+
+import Document, { Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
+export default class MyDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage(App => props =>
+      sheet.collectStyles(<App {...props} />)
+    );
+    const styleTags = sheet.getStyleElement();
+    return { ...page, styleTags };
+  }
+
+  render() {
+    return (
+      <html>
+        <Head>{this.props.styleTags}</Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
