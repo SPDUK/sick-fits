c5d7414d044d73b7a47b5a59dc170e75d3504052
SPDUK <spdevuk@gmail.com>
2019-02-19 19:39:57 +0000

    set up graphql-yoga server

---
+++ backend/src/createServer.js
@@ -0,0 +1,31 @@
+// set up graphQL yoga (express server)
+
+const { GraphQLServer } = require('graphql-yoga');
+// resolvers find where the data comes from
+// query resolvers -> get data
+// mutation resolvers -> push data
+
+const Mutation = require('./resolvers/Mutation');
+const Query = require('./resolvers/Query');
+const db = require('./db');
+
+// create the graphQL yoga server
+
+function createServer() {
+  // needs typedefs for backend graphQL server
+  return new GraphQLServer({
+    typeDefs: 'src/schema.graphql',
+    // match up everything in the schema witha mutation or query resolver
+    resolvers: {
+      Mutation,
+      Query,
+    },
+    resolverValidationOptions: {
+      requireResolversForResolveType: false,
+    },
+    // allowing access the database from the resolvers for every request
+    context: req => ({ ...req, db }),
+  });
+}
+
+module.exports = createServer;
---
+++ backend/src/db.js
@@ -0,0 +1,11 @@
+const { Prisma } = require('prisma-binding');
+// connects to the remote prisma DB and gives up the ability to query it with JS
+// pull down the prisma.graphql file from the DB and pass it in as typeDefs
+const db = new Prisma({
+  typeDefs: 'src/generated/prisma.graphql',
+  endpoint: process.env.PRISMA_ENDPOINT,
+  secret: process.env.PRISMA_SECRET,
+  debug: true,
+});
+
+module.exports = db;
