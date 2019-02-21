28edcca78df497b1d0613b9e2faaaaec0edbd109
SPDUK <spdevuk@gmail.com>
2019-02-21 19:00:46 +0000

    adding deleteItem (no auth)

---
+++ frontend/components/DeleteItem.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
+import { ALL_ITEMS_QUERY } from './Items';
+
+const DELETE_ITEM_MUTATION = gql`
+  mutation DELETE_ITEM_MUTATION($id: ID!) {
+    deleteItem(id: $id) {
+      id
+    }
+  }
+`;
+
+export default class DeleteItem extends Component {
+  // apollo gives 2 things when updating, cache and payload (data that comes back)
+  update = (cache, payload) => {
+    // manually update cache on client to match server
+    // 1. Read cache for items we want
+    const data = cache.readQuery({ query: ALL_ITEMS_QUERY });
+    console.log(data, payload);
+    // 2. Filter the delete item out of the page
+    data.items = data.items.filter(
+      item => item.id !== payload.data.deleteItem.id
+    );
+    // 3. put items back
+    cache.writeQuery({ query: ALL_ITEMS_QUERY, data });
+  };
+
+  render() {
+    const { id, children } = this.props;
+    return (
+      <Mutation
+        variables={{ id }}
+        mutation={DELETE_ITEM_MUTATION}
+        update={this.update}
+      >
+        {(deleteItem, { error }) => (
+          <button
+            type="button"
+            onClick={() => {
+              if (confirm('Are you sure you want to delete this?'))
+                deleteItem();
+            }}
+          >
+            {children}
+          </button>
+        )}
+      </Mutation>
+    );
+  }
+}
