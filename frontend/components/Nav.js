7f08de38658b38a0c0d8ff961580ca0e8a92dbba
SPDUK <spdevuk@gmail.com>
2019-02-22 15:52:15 +0000

    adding user signin

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -4,28 +4,32 @@ import User from './User';
 
 // destructures data out of the response to User, then me out of the data
 const Nav = () => (
-  <NavStyles>
-    <User>
-      {({ data: { me } }) => {
-        if (me.name) return <p> {me.name}</p>;
-        return null;
-      }}
-    </User>
-    <Link href="/items">
-      <a>Shop</a>
-    </Link>
-    <Link href="/sell">
-      <a>Sell</a>
-    </Link>
-    <Link href="/signup">
-      <a>Sign Up</a>
-    </Link>
-    <Link href="/orders">
-      <a>Orders</a>
-    </Link>
-    <Link href="/me">
-      <a>Accounts</a>
-    </Link>
-  </NavStyles>
+  <User>
+    {({ data: { me } }) => (
+      <NavStyles>
+        <Link href="/items">
+          <a>Shop</a>
+        </Link>
+        {me && (
+          <>
+            <Link href="/sell">
+              <a>Sell</a>
+            </Link>
+            <Link href="/orders">
+              <a>Orders</a>
+            </Link>
+            <Link href="/me">
+              <a>Accounts</a>
+            </Link>
+          </>
+        )}
+        {!me && (
+          <Link href="/signup">
+            <a>Sign In</a>
+          </Link>
+        )}
+      </NavStyles>
+    )}
+  </User>
 );
 export default Nav;
