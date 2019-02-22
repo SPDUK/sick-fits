b00df779b320049f943a89a8a396aa1f48095f8a
SPDUK <spdevuk@gmail.com>
2019-02-22 15:51:56 +0000

    adding refetchQueries to signup to re-render the signed in user

--- frontend/components/Signup.js
+++ frontend/components/Signup.js
@@ -3,6 +3,7 @@ import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
 import Form from './styles/Form';
 import Error from './ErrorMessage';
+import { CURRENT_USER_QUERY } from './User';
 
 const SIGNUP_MUTATION = gql`
   mutation SIGNUP_MUTATION(
@@ -31,7 +32,11 @@ export default class Signup extends Component {
   render() {
     const { email, name, password } = this.state;
     return (
-      <Mutation mutation={SIGNUP_MUTATION} variables={this.state}>
+      <Mutation
+        mutation={SIGNUP_MUTATION}
+        variables={this.state}
+        refetchQueries={[{ query: CURRENT_USER_QUERY }]}
+      >
         {(signUp, { error, loading }) => (
           <Form
             method="post"
