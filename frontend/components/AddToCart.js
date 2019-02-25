a2842ee500872424f9e2a2dd26f7d5f531fdde45
SPDUK <spdevuk@gmail.com>
2019-02-25 18:03:10 +0000

    adding CartItem and updating User mutation to display cart info

--- frontend/components/AddToCart.js
+++ frontend/components/AddToCart.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
+import { CURRENT_USER_QUERY } from './User';
 
 const ADD_TO_CART_MUTATION = gql`
   mutation addToCart($id: ID!) {
@@ -14,10 +15,14 @@ export default class AddToCart extends Component {
   render() {
     const { id } = this.props;
     return (
-      <Mutation mutation={ADD_TO_CART_MUTATION} variables={{ id }}>
-        {addToCart => (
-          <button onClick={addToCart} type="button">
-            Add to cart!
+      <Mutation
+        mutation={ADD_TO_CART_MUTATION}
+        variables={{ id }}
+        refetchQueries={[{ query: CURRENT_USER_QUERY }]}
+      >
+        {(addToCart, { error, loading }) => (
+          <button disabled={loading} onClick={addToCart} type="button">
+            Add{loading && 'ing'} To Cart
           </button>
         )}
       </Mutation>
