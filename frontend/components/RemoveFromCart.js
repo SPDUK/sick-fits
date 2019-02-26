534a9e47db6332f85e6117dc9b79d4d706371f23
SPDUK <spdevuk@gmail.com>
2019-02-26 11:53:04 +0000

    Updating RemoveFromCart to  remove an item from the cart using optimisticResponse

--- frontend/components/RemoveFromCart.js
+++ frontend/components/RemoveFromCart.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import gql from 'graphql-tag';
-// import { CURRENT_USER_QUERY } from './User';
+import { CURRENT_USER_QUERY } from './User';
 
 const REMOVE_FROM_CART_MUTATION = gql`
   mutation removeFromCart($id: ID!) {
@@ -27,10 +27,32 @@ export default class RemoveFromCart extends Component {
     id: PropTypes.string.isRequired,
   };
 
+  // gets called as soon as we get a response BACK from the server after a mutation finishes
+  // cache = apollo cache, payload = result back from the server
+  update = (cache, payload) => {
+    const data = cache.readQuery({ query: CURRENT_USER_QUERY });
+
+    const cartItemId = payload.data.removeFromCart.id;
+    data.me.cart = data.me.cart.filter(cartItem => cartItem.id !== cartItemId);
+
+    cache.writeQuery({ query: CURRENT_USER_QUERY, data });
+  };
+
   render() {
     const { id } = this.props;
     return (
-      <Mutation variables={{ id }} mutation={REMOVE_FROM_CART_MUTATION}>
+      <Mutation
+        variables={{ id }}
+        mutation={REMOVE_FROM_CART_MUTATION}
+        update={this.update}
+        optimisticResponse={{
+          __typename: 'Mutation',
+          removeFromCart: {
+            __typename: 'CartItem',
+            id: this.props.id,
+          },
+        }}
+      >
         {(removeFromCart, { loading, error }) => (
           <BigButton
             title="Delete Item"
